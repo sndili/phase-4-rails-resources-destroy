59e05f3b4768e10044769b015b504a9e32f89906
Sylvia Ndili <ndilisylvia@gmail.com>
2022-11-21 16:17:15 +0300

    completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -44,6 +44,17 @@ class BirdsController < ApplicationController
     end
   end
 
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      # head 202
+      render json: {}
+    else
+      render json: {error: "Bird not found"}, status: 404
+    end
+  end
+
   private
 
   def bird_params
